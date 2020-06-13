0639057a52aa954b9cb6c43dbdd6a7782c38db0a
Mihaela Mateescu <miha.mateescu@gmail.com>
2020-06-13 15:26:59 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Switch, Route, NavLink } from 'react-router-dom';
+import CountryDetails from './components/CountryDetails';
+import CountriesList from './components/CountriesList';
+import countries from './countries.json';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  getBorders = country => {
+    const countryBorders = country.borders;
+    return countryBorders.map((coun, index) => {
+      const border = countries.find(country => country.cca3 === coun);
+      return (
+        <li key={`${index + 1}-country--listItem`}>
+          <NavLink to={`/country/${border.cca3}`}>{border.name.common}</NavLink>
+        </li>
+      );
+    });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <CountriesList countries={countries} />
+          <Switch>
+            <Route
+              exact
+              path="/country/:id"
+              render={props => (
+                <CountryDetails
+                  {...props}
+                  countries={countries}
+                  getBorders={this.getBorders}
+                />
+              )}
+            />
+          </Switch>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/CountryDetails.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const CountryDetails = ({ countries, getBorders, ...props }) => {
+  const country = countries.find(coun => coun.cca3 === props.match.params.id);
+  console.log({ country });
+  return (
+    <div className="col-7">
+      <div className="table">
+        <h2>{country.name.common}</h2>
+        <h3 style={{ width: '30%' }}>{`Capital: ${country.capital[0]}`}</h3>
+        <h3>{`Area: ${country.area} Km2`}</h3>
+        <h3>Borders:</h3>
+        <ul>{getBorders(country)}</ul>
+      </div>
+    </div>
+  );
+};
+
+export default CountryDetails;
